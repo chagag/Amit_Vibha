71a46eae780579cf038eee05923196357b58ac4b
Vibha <puri16v@gmail.com>
2019-01-15 17:12:41 -0800

    Updated combinatorics model, output files

--- model_bias_parameter/combinatorics.py
+++ model_bias_parameter/combinatorics.py
@@ -1,6 +1,7 @@
 import os.path
 import pandas as pd
 import itertools
+import statistics
 
 DATA_PATH = '../'
 
@@ -16,21 +17,39 @@ def combinatoric(agent_scores):
 	sums = sorted(sums)
 	return sums
 
+def maximize_homophily(my_rating, agent_scores):
+	agent_scores = list(agent_scores.values())
+	scores = []
+	#Find top 3 closest
+	for i in range(3):
+		closest = min(agent_scores, key=lambda x:abs(x - my_rating))
+		scores.append(closest)
+		agent_scores.remove(closest)
+	return sum(scores)
+
+def find_z(picked, forced_mean, stdev):
+	z = (picked - forced_mean)/stdev
+	return z
+
 def simulation(subjID):
 	me_path = DATA_PATH + subjID + '.csv'
 	me = pd.read_csv(me_path)
+
+	results = pd.DataFrame()
 
 	for i in range(len(me)):
 		#Get all *seen* agent scores for this stimulus
 		agent_scores = dict()
 		for j in range(1, 7):
 			agent_scores[me['a%s_name'%j][i]] = me['a%s_rating'%j][i]
-		print("Seen agents:")
-		print(agent_scores)
+		# print("Seen agents:")
+		# print(agent_scores)
+
 		#Find all combinatoric possiblities
 		possible_sums = combinatoric(agent_scores)
-		print("Possible sums:")
-		print(possible_sums)
+		# print("Possible sums:")
+		# print(possible_sums)
+
 		#Find picked possibility
 		picked_agents = dict()
 		for j in range(1, 4):
@@ -40,19 +59,46 @@ def simulation(subjID):
 		picked_sum = 0
 		for j in range(len(picked_scores)):
 			picked_sum += picked_scores[j]
-		print("Picked agents:")
-		print(picked_agents)
-		print("Picked sum:")
-		print(picked_sum)
+		# print("Picked agents:")
+		# print(picked_agents)
+		# print("Picked sum:")
+		# print(picked_sum)
 
-		#Find ranking in comparison
-		rank = 0
-		for j in range(len(possible_sums)):
-			if possible_sums[j] < picked_sum:
-				rank += 1
-		print("Ranking:")
-		print(rank)
+		#Find what would have picked if maximizing homophily
+		my_rating = me['subject_rating'][i]
+		# print("My rating:")
+		# print(my_rating)
+		maxhomo_score = maximize_homophily(my_rating, agent_scores)
+		# print("If trying to maximize homophily:")
+		# print(maxhomo_score)
+
+		#Find ranking in comparison to minimum
+		min_rank = possible_sums.index(picked_sum)
+		# print("Ranking wrt minimum:")
+		# print(min_rank)
+
+		#Find ranking in comparison to maximizing homophily
+		maxhomo_index = possible_sums.index(maxhomo_score)
+		maxhomo_rank = min_rank - maxhomo_index #this is the index of the picked sum
+		# print("Ranking wrt maximizing homophily:")
+		# print(maxhomo_rank)
+
+		#Find z score wrt maximizing homophily as forced mean
+		stdev = statistics.stdev(possible_sums)
+		mean = statistics.mean(possible_sums)
+		z = find_z(picked_sum, maxhomo_score, stdev)
+		# print("Z Score wrt maximizing homophily:")
+		# print(z)
+
+		results = results.append(pd.Series([mean, stdev, picked_sum, possible_sums[0], min_rank, maxhomo_score, maxhomo_rank, z]), ignore_index = True)
+
+	results.columns = ["Mean of Possible Sums", "Stdev of Possible Sums", "Picked Sum", "Minimum Possible Sum", "Ranking From Min", "Maximizing Homophily Sum", "Ranking from Max. Homo.", "Z Score from Max. Homo."]
+	results.to_csv("output/%s_bias.csv"%subjID, index = False)
+	# print(results)
 
 #Can iterate through all subjects to produce some output sheet, but check above first
-simulation('3')
+if __name__ == '__main__':
+	for i in range(91):
+	    if os.path.isfile(DATA_PATH+str(i)+'.csv'):
+	        simulation(str(i))
 
